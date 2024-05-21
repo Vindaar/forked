5e18deba9e7ce037adec6c05a53333005a6464eb
Vindaar <basti90@gmail.com>
2024-05-21 19:50:20 +0200

    clean up code a bit, support access to iteration index

--- forked.nim
+++ forked.nim
@@ -21,7 +21,8 @@ type
     id: NimNode # identfier or expression to be sent
     n: NimNode
   IterStmt = object
-    n: NimNode
+    idx: NimNode ## possible iteration index (optional)
+    sId: NimNode ## iteration variable
     iter: NimNode ## the actual iterator
     jobs: NimNode
 
@@ -35,7 +36,6 @@ const BasePath* {.strdefine.} = "/dev/shm/pp_forked/"
 
 template send*(oSym, idxSym, arg: untyped): untyped {.dirty.} =
   ## This template takes care of writing data from each job back to the main process
-  #echo "TYPE: ", typeof(arg), " support?", typeof(arg).supportsCopyMem
   when not typeof(arg).supportsCopyMem: #needsCopy:
     when WriteFiles:
       # We create a path based on the current PID. Because this runs in the forked process, it means
@@ -44,7 +44,7 @@ template send*(oSym, idxSym, arg: untyped): untyped {.dirty.} =
       if not dirExists(dir):
         createDir(dir)
       let path = dir / "job_" & $idxSym & ".dat"
-      arg.saveBuffer(path)
+      (idxSym, arg).saveBuffer(path)
       let n = path.len
       discard oSym.uriteBuffer(n.addr, n.sizeof) # could use a different way to write, but would need other `frame*` logic
       discard oSym.uriteBuffer(path[0].addr, n)
@@ -54,7 +54,8 @@ template send*(oSym, idxSym, arg: untyped): untyped {.dirty.} =
       discard oSym.uriteBuffer(n.addr, n.sizeof)
       discard oSym.uriteBuffer(buf.data, buf.size)
   else:
-    discard oSym.uWr(arg)
+    var mut = (idxSym, arg) ## XXX: ideally `uWr` doesn't need `var T`
+    discard oSym.uWr(mut)
 
 ## The two helper templates which produce the correct procpool, depending on the
 ## data types used
@@ -69,7 +70,7 @@ template ppFramesOb(R, W, oSym, sSym, idxSym, jobs, body: untyped): untyped =
         body
   ), framesOb, jobs, aux = sizeof(W))
 
-template ppFramesLenPfx(R, W, oSym, sSym, idxSym, jobs, body: untyped): untyped =
+template ppFramesLenPfx(R, oSym, sSym, idxSym, jobs, body: untyped): untyped =
   initProcPool( (
     proc(r, w: cint) =
       let i = open(r)
@@ -80,45 +81,52 @@ template ppFramesLenPfx(R, W, oSym, sSym, idxSym, jobs, body: untyped): untyped 
         body
   ), framesLenPfx, jobs)
 
-proc extractJoin(n: NimNode): JoinStmt =
+proc extractJoin(body: NimNode): JoinStmt =
   ## Extract the `finally:` statement from the ForLoop
-  doAssert n.kind == nnkForStmt
-  doAssert n.len == 3
-  doAssert n[2].kind == nnkStmtList
-  let body = n[2]
+  doAssert body.kind == nnkStmtList
   let join = body[body.len-1]
   if join.kind notin {nnkCall, nnkCommand} and
     join[0].kind != nnkIdent and join[0].strVal != "join":
     error("The last stmt inside the `forked` for loop must be a `join` statement! Is: " & $body[body.len-1].treerepr)
   result = JoinStmt(n: join[1])
 
-proc extractSend(n: NimNode): SendStmt =
+proc extractSend(body: NimNode): SendStmt =
   ## Extract the `send` statement from the ForLoop
-  doAssert n.kind == nnkForStmt
-  doAssert n.len == 3
-  doAssert n[2].kind == nnkStmtList
-  let body = n[2]
+  doAssert body.kind == nnkStmtList
   if body[body.len-2].kind notin {nnkCommand, nnkCall}:
     error("The second to last stmt inside the `forked` for loop must be a `send foo` stmt!")
   let sen = body[body.len-2]
   doAssert sen[0].kind in {nnkSym, nnkIdent} and sen[0].strVal == "send"
   result = SendStmt(n: sen, id: sen[1])
 
-proc extractBody(n: NimNode): NimNode =
+proc extractBody(body: NimNode): NimNode =
   ## Extract the actual body of the ForLoop that should run for each job
-  doAssert n.kind == nnkForStmt
-  doAssert n.len == 3
-  doAssert n[2].kind == nnkStmtList
-  let body = n[2]
+  doAssert body.kind == nnkStmtList
   result = newStmtList()
   for i in 0 ..< body.len - 2:
     result.add body[i]
 
-proc extractIter(n: NimNode): IterStmt =
+proc extractIter(syms: seq[NimNode], call: NimNode): IterStmt =
   ## Extract the actual iterator we iterate over, as well as the, unfortunately needed
   ## output type information
-  doAssert n.kind == nnkForStmt
-  let call = n[1]
+  var
+    sId: NimNode
+    idx = genSym(nskLet, "idx")
+  case syms.len
+  of 1: sId = syms[0]
+  of 2: (idx, sId) = (syms[0], syms[1])
+  else:
+    error("""Invalid arguments to `forked`. Either 1 or 2 arguments,
+for x in forked(...)
+or
+for i, x in forked(...)
+but got: """ & $syms.repr)
+
+  # `sId` might be a `(i, x)` node. Maybe unpack
+  if sId.kind == nnkVarTuple:
+    doAssert sId.len == 3, "nnkVarTuple node in `forked` must be a tuple of only 2 " &
+      "elements, but got " & $(sId.len - 1) # `nnkVarTuple` has 1 empty node if no types
+    (idx, sId) = (sId[0], sId[1])
   doAssert call.kind == nnkCall and call[0].kind == nnkIdent and
     call[0].strVal == "forked", "For loop argument must be a `forked` call"
   var jobs = newLit 0
@@ -127,29 +135,25 @@ proc extractIter(n: NimNode): IterStmt =
   of 3: jobs = call[2] # get number of jobs from last argument
   else:
     error("Must be length 2, (forked, iter). Got: " & $call.treerepr)
-  result = IterStmt(n: n, iter: call[1], jobs: jobs)
+  result = IterStmt(sId: sId, idx: idx, iter: call[1], jobs: jobs)
 
-proc replaceSymbol(n: NimNode, arg, by: NimNode): NimNode =
-  ## Helper to replace a symbol `arg` by `by` in the given
-  ## NimNode `n`
-  proc finish(n: NimNode, arg, by: NimNode): NimNode =
-    case n.kind
-    of nnkIdent:
-      if n.strVal == arg.strVal: result = by
-      else: result = n
-    else:
-      if n.len == 0: result = n
-      else:
-        result = newTree(n.kind)
-        for ch in n:
-          result.add finish(ch, arg, by)
-  result = finish(n, arg, by)
+proc deconstructForStmt(n: NimNode): tuple[syms: seq[NimNode],
+                                           call: NimNode,
+                                           body: NimNode] =
+  doAssert n.kind == nnkForStmt
+  var syms = newSeq[NimNode]()
+  # 1 argument:  `for x in forked(...)`
+  # 2 arguments: `for i, x in forked(...)`
+  for i in 0 ..< n.len - 2:
+    doAssert n[i].kind in {nnkIdent, nnkVarTuple}
+    syms.add n[i]
+  result = (syms: syms, call: n[n.len - 2], body: n[n.len - 1])
 
-proc finalizeJoin(fn: JoinStmt, send: SendStmt, outTyp, joinId: NimNode): NimNode =
+proc finalizeJoin(fn: JoinStmt, send: SendStmt, outTyp, joinId, idxSym: NimNode): NimNode =
   ## Replace the `send` identifier by a correct pointer cast / load from buffer
   ## setup
   let sl = genSym(nskParam, "slice") ## XXX: update
-  let by = quote do:
+  let data = quote do: # load data / extract from MSlice
     when not `outTyp`.supportsCopyMem:
       when WriteFiles:
         let path = $(`sl`)
@@ -159,7 +163,14 @@ proc finalizeJoin(fn: JoinStmt, send: SendStmt, outTyp, joinId: NimNode): NimNod
         flatTo[`outTyp`](buf)
     else:
       cast[ptr `outTyp`](`sl`.mem)[]
-  result = replaceSymbol(fn.n, send.id, by)
+  let senId = send.id
+  let varDec = quote do:
+    let (`idxSym`, `senId`) = `data` # unpack the (job index, user data)
+  ## Produce a template, which unpacks the received data and then
+  ## performs user desired operation in `join:`
+  result = newStmtList()
+  result.add varDec
+  result.add fn.n
   result = quote do: # produce the final template, arg to `onReply`
     template `joinId`(`sl`: MSlice) =
       `result`
@@ -170,47 +181,51 @@ proc patchSendTmpl(sen: SendStmt, oId, idxSym: NimNode): NimNode =
 
 macro forked*(n: ForLoopStmt): untyped =
   ## XXX: Extend to not force `evalOb` and `uRd` on the input side! Also allow copy & load there
+  # Deconstruct the `ForLoopStmt` into its pieces
+  let (syms, call, body) = deconstructForStmt(n)
   # Extract all the relevant pieces of the for loop stmt
-  let iter = extractIter(n)
-  let join = extractJoin(n)
-  let sen  = extractSend(n)
-  var body = extractBody(n)
-  let sId  = n[0] # iterator variable ## TODO: allow pair of (i, x)
+  let iter     = extractIter(syms, call)
+  let join     = extractJoin(body)
+  let sen      = extractSend(body)
+  var loopBody = extractBody(body)
 
   # Define the symbols to use in the generated code
   let oId    = genSym(nskLet, "o")
   let joinId = genSym(nskTemplate, "join")
   let ppId   = genSym(nskVar, "pp")
-  let idxSym = genSym(nskLet, "idx")
+  let Wid    = genSym(nskType, "W") # write type (without index!)
 
-  # Get iterator and symbol of output type
+  # Get iterator, and iteration variables for loop
   let iterCall = iter.iter
-  let Wid      = genSym(nskType, "W") # write type
+  let sId      = iter.sId
+  let idxSym   = iter.idx
 
   # Produce a "fake" body that we use to determine the type of the
   # data we `send`
   var fakeBody = quote do:
     type RIn = typeof(`iterCall`)
     var `sId`: RIn
-  fakeBody.add body.copy()
+    let `idxSym` = 0
+  fakeBody.add loopBody.copy()
   let senId = sen.id
   fakeBody.add senId
 
   # finalize the `join` `onReply` logic
-  let finJoin  = finalizeJoin(join, sen, Wid, joinId)
+  let finJoin  = finalizeJoin(join, sen, Wid, joinId, idxSym)
 
   # finalize the body (i.e. patch `send` template)
-  body.add patchSendTmpl(sen, oId, idxSym)
+  loopBody.add patchSendTmpl(sen, oId, idxSym)
   let jobs = iter.jobs
   result = quote do:
-    type RIn = typeof(`iterCall`)
-    type R = (int, RIn)
-    type `WId` = typeof(`fakeBody`)
+    type RIn = typeof(`iterCall`) # parent ⇒ child type (user)
+    type R = (int, RIn)           # actual type we transfer (job index, user data)
+    type WIn = typeof(`fakeBody`) # child ⇒ parent type (user)
+    type `WId` = (int, Win)       # actual type we transfer (job index, user data)
     let jobs = if `jobs` > 0: `jobs` else: parseInt(getEnv("PP_JOBS", $countProcessors()))
     when not `WId`.supportsCopyMem:
-      var `ppId` = ppFramesLenPfx(R, `WId`, `oId`, `sId`, `idxSym`, jobs, `body`)
+      var `ppId` = ppFramesLenPfx(R, `oId`, `sId`, `idxSym`, jobs, `loopBody`)
     else:
-      var `ppId` = ppFramesOb(R, `WId`, `oId`, `sId`, `idxSym`, jobs, `body`)
+      var `ppId` = ppFramesOb(R, `WId`, `oId`, `sId`, `idxSym`, jobs, `loopBody`)
     `finJoin`
     iterator inner(): R =
       var idx = 0
@@ -243,10 +258,11 @@ when isMainModule:
 
   proc calcFoo(x: int): Foo = Foo(s: $x, x: x.float)
   proc testIt(): seq[Foo] =
-    for x in forked(0 ..< 10):
+    result = newSeq[Foo](10)
+    for i, x in forked(0 ..< 10):
       let f = calcFoo(x)
       send f
-      join: result.add f
+      join: result[i] = f
 
   echo piProcPool(100)
   echo testIt()
